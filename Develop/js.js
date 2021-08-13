25f7a4b6e5f07ed1d437b541008a98ab54d992fa
Jon Snow <84160178+Sethmarks216@users.noreply.github.com>
2021-08-13 14:42:28 -0400

    it's workign!! getting the hour fomat to work

--- Develop/js.js
+++ Develop/js.js
@@ -1,41 +1,79 @@
+//sets current time in header area
 var date = moment().format('MMM Do YYYY, h:mm:ss a');
-
 var dateEL = document.querySelector("#date");
-
 dateEL.textContent = date;
 
+//loop thru all time block html elements
 var timeblockElements = document.querySelectorAll(".time-block");
-
 for (let i = 0; i < timeblockElements.length; i++) {
-    const element = timeblockElements[i];
-    const hourHtmlString = element.querySelector(".hour").innerHTML;
-    const hourValueAsString = hourHtmlString.match(/\d+/g)[0];
-    var hourValue = parseInt(hourValueAsString);
-    var hourValueAsTimeString = "T";
-    const isAm = hourHtmlString.includes("am");
-
-    if(!isAm) {
-        hourValue + 12;
-    } 
     
-    if(hourValue < 10) {
-        hourValueAsTimeString = "0" + hourValue.toString();
-    } else {
-        hourValueAsTimeString = hourValue.toString();
-    }
-
+    //get current date values
     var currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth() + 1;
     const currentDay = currentDate.getDate();
 
-    console.log(hourValueAsTimeString);
-    var momentDate = moment(
-            currentYear + "-" + 
-            currentMonth + "-" + 
-            currentDay +
-            hourValueAsTimeString)
-            .format("YYYY-MM-DDTHH");
+    //gets time value as number from html
+    const element = timeblockElements[i];
+    const hourHtmlString = element.querySelector(".hour").innerHTML;
 
-    console.log("momentDate", momentDate);
+    //strip non-digit characters from html hour string e.g. 9am or 2pm
+    const hourValueAsString = hourHtmlString.match(/\d+/g)[0];
+    var hourValue = parseInt(hourValueAsString);
+
+    //convert hour number to 24 hour clock
+    const isAm = hourHtmlString.includes("am");
+    if(!isAm) {
+        hourValue = hourValue + 12;
+    } 
+
+    //format hour number as string for moment date
+    var hourValueAsTimeString = hourValue.toString();
+    if(hourValue < 10) {
+        hourValueAsTimeString = 
+            addLeadingZeroToString(hourValue.toString());
+    }
+    
+    //format month number as string for moment date
+    var monthValueAsString = currentMonth.toString();
+    if(currentMonth < 10) {
+        monthValueAsString = 
+            addLeadingZeroToString(currentMonth.toString());
+    }
+    
+    const dateString = 
+        currentYear.toString() + "-" + 
+        monthValueAsString + "-" + 
+        currentDay.toString() +
+        " " + hourValueAsTimeString + ":00";
+    var timeBlockDate = moment(dateString, "YYYY-MM-DD HH:mm");
+
+    /**
+     * set timeblock color using helper 
+     * css classes: past, present, or future
+     *  */
+
+    var timeBlockHour = timeBlockDate.hour();
+    var momentHour = moment().hour();
+    if(momentHour < 10) {
+        momentHour = momentHour + 12;
+    } 
+
+    //console.log('timeBlockHour:', timeBlockHour);
+    //console.log('momentHour:', momentHour);
+    
+    var textArea = element.querySelector("textarea");
+    if(timeBlockHour < momentHour) {
+        textArea.setAttribute("class", "description col-8 past");
+    } else if (timeBlockHour == momentHour) {
+        textArea.setAttribute("class", "description col-8 present");
+    } else {
+        textArea.setAttribute("class", "description col-8 future");
+    }
+}
+
+function addLeadingZeroToString(_data) {
+    if(_data != null) {
+        return "0" + _data;
+    }   
 }
